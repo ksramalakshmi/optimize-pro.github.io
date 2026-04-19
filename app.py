614b44ac2ace9dc617920c2f9d389c774d110262
ksramalakshmi <rlsri2305@gmail.com>
2026-04-19 12:06:36 +0530

    Fix port binding for Render & add gunicorn

--- app.py
+++ app.py
@@ -93,4 +93,6 @@ def _seed_defaults(app):
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port, debug=False)
